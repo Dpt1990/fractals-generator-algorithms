4db5bbb578742d9df4d705efd2b1877a1fd1f7f0
Dpt90 <37405083+Dpt1990@users.noreply.github.com>
2024-04-22 17:58:14 +0000

    Aggiunta menù e frattale
    é stato rifattorizzato con un menù da linea di comando per la scelta dei frattali da riprodurre

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-import noise
-import numpy as np
-import matplotlib.pyplot as plt
+import fractal
+import pygame
 
-# Dimensioni del rumore
-figure = (200, 200)
+def menu_principale():
+  scelta = 0
+  while (scelta != "2"):
+    scelta = input("Digita 1 per stampare 'il frattale di Koch' o 2 per stampare 'il frattale': ")
+    if scelta == "1":
+        fractal.frattale_koch()
+    elif scelta == "2":
+        pygame.quit()
+    else:
+      print("Scelta non valida")
 
-# Fattore di scala
-dimension = 100.0
-
-# Generazione della griglia di coordinate
-def generate_perlin_noise(figure, dimension):
-    world = np.zeros(figure)
-    for i in range(figure[0]):
-        for j in range(figure[1]):
-            world[i][j] = noise.pnoise2(i/dimension, 
-                                         j/dimension, 
-                                         octaves=6, #le ottave servono ad indicare quanti livelli di rumore generare, in modo da aggiungere più dettagli 
-                                         persistence=0.5, 
-                                        #la persistenza indica quanto influiscono le ottave successive sul rumore generato
-                                         lacunarity=2.0, 
-                                        #indica la frequenza delle variazioni del rumore cambia ad ogni ottava. La lacunarità indica quanto più grande sarà la variazione del rumore.
-                                         repeatx=1024, 
-                                         repeaty=1024, 
-                                         base=0)
-    return world
-
-# Generazione del rumore
-world = generate_perlin_noise(figure, dimension)
-
-# Visualizzazione del rumore generato
-plt.imshow(world, cmap='gray', interpolation='nearest')
-plt.colorbar()
-plt.show()
+if __name__ == "__main__":
+    menu_principale()
